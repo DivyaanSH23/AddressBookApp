77850c5992884da40e57a82d2e14f84ee2bfb1a9
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-03-06 11:55:27 +0530

    Setup for AddressBook App

---
+++ src/test/java/com/bridgeLabz/AddressBookApp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.AddressBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
